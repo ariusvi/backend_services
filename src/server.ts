4812a2c95029fe9f40e6e78ccd0604f06e83d0a7
Ana Rius <a.riusvi11@gmail.com>
2024-02-26 21:03:07 +0100

    feat:dotenv library added

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 4001;
+
 app.listen(
-    4000, () => {
+    PORT, () => {
         console.log("Server is running")
     }
 )
