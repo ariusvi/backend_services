b2d539a3d42a9b78d767234055df8d2b8428afba
Ana Rius <a.riusvi11@gmail.com>
2024-02-26 22:39:23 +0100

    feat: refactpr healthy endpoint

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,15 @@ const app = express();
 
 const PORT = process.env.PORT || 4001;
 
+app.get('/healthy', (req, res) => {         /*para ver si la api está viva*/
+    res.status(200).json(
+        {
+            success: true,
+            message: "Server is healthy"
+        }
+    );
+})
+
 app.listen(
     PORT, () => {
         console.log(`Server is running on port: ${PORT}`)
