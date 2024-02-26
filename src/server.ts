93b1571d99021eb6a5445fd25ea6a8ff45178e89
Ana Rius <a.riusvi11@gmail.com>
2024-02-26 22:27:55 +0100

    feat:healthy endpoint

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,6 @@ const PORT = process.env.PORT || 4001;
 
 app.listen(
     PORT, () => {
-        console.log("Server is running")
+        console.log(`Server is running on port: ${PORT}`)
     }
 )
