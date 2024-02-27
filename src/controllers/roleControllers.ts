ccde6c0bfa3d2cefb006c31a78fbe0df5f91cf00
Ana Rius <a.riusvi11@gmail.com>
2024-02-27 21:18:22 +0100

    feat:update role

---
+++ src/controllers/roleControllers.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express"
+
+export const getRoles = (req: Request, res: Response) => {
+    res.status(200).json(
+        {
+            success: true,
+            message: "Roles retrieved succcessfully"
+        }
+    )
+}
+
+export const createRole = (req: Request, res: Response) => {
+    res.status(201).json(
+        {
+            success: true,
+            message: "Role created"
+        }
+    )
+}
+
+export const updateRole = (req: Request, res: Response) => {
+    res.status(200).json(
+        {
+            success: true,
+            message: "Role updated"
+        }
+    )
+}
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 import { AppDataSource } from "./database/db";
+import { createRole, getRoles, updateRole } from "./controllers/roleControllers";
 
 
 
@@ -10,13 +11,18 @@ const app = express();
 
 const PORT = process.env.PORT || 4001;
 
-app.get("/healthy", (req: Request, res: Response) => {
+app.get("/healthy", (req, res) => {
     /*para ver si la api está viva*/
     res.status(200).json({
         success: true,
         message: "Server is healthy",
     });
 });
+
+/*roles routes*/
+app.get('/roles', getRoles)
+app.post('/roles', createRole)
+app.put('/roles', updateRole)
 
 AppDataSource.initialize()
     .then(() => {
