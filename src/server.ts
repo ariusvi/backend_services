5aee84aec5819351ef22d367fdb9c60f36b9831b
Ana Rius <a.riusvi11@gmail.com>
2024-02-27 09:30:40 +0100

    feat: conexion data base

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { AppDataSource } from "../database/db";
 
 dotenv.config();
 
@@ -7,17 +8,22 @@ const app = express();
 
 const PORT = process.env.PORT || 4001;
 
-app.get('/healthy', (req, res) => {         /*para ver si la api está viva*/
-    res.status(200).json(
-        {
-            success: true,
-            message: "Server is healthy"
-        }
-    );
-})
+app.get("/healthy", (req, res) => {
+    /*para ver si la api está viva*/
+    res.status(200).json({
+        success: true,
+        message: "Server is healthy",
+    });
+});
 
-app.listen(
-    PORT, () => {
-        console.log(`Server is running on port: ${PORT}`)
-    }
-)
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Database connected");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
